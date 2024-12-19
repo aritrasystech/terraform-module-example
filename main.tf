0c322ed66abe70e6b90a9166b855ff647e3ba147
Aritra Bhattacharya <77916826+aritrasystech@users.noreply.github.com>
2024-12-19 15:23:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 resource "aws_instance" "webserver" {
   ami           = "ami-0a4408457f9a03be3"
   instance_type = "t2.micro"
