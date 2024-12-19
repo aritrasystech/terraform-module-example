8cb5c73fb4b66c2f38217cc44707effe510ced2f
Aritra Bhattacharya <77916826+aritrasystech@users.noreply.github.com>
2024-12-19 15:20:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIAY7DF6MZ26IZRZ67V"
-  secret_key = "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"
-}
-
 resource "aws_instance" "webserver" {
   ami           = "ami-0a4408457f9a03be3"
   instance_type = "t2.micro"
